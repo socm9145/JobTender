5e6ee283f55053a6d712fd85f705418fd9e2db6f
dongsu712 <dongsu712@naver.com>
2023-03-14 09:28:37 +0900

    [Feat] Kakao Login Button
    Former-commit-id: cfc1fd4f908c4821df9b197a6cb068ed57ed7cff

---
+++ src/components/login/KakaoButton.js
@@ -0,0 +1,16 @@
+const KakaoButton = () => {
+  return (
+    <div>
+      <li>
+        <a id="custom-login-btn">
+          <img
+            src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
+            width="242"
+          />
+        </a>
+      </li>
+    </div>
+  );
+};
+
+export default KakaoButton;
