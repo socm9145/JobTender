3f67bef1e1a74dcd52607d7c3d04c305d9517291
유정훈 <choanury@naver.com>
2023-03-15 17:59:13 +0900

    [Feat] Homepage 제작중
    Former-commit-id: 829e0096f11871b8d26bb4c48440c8af00ac6c45

--- frontend/src/pages/router/Home.js
+++ frontend/src/pages/router/Home.js
@@ -1,8 +1,57 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
+  useEffect(() => {
+    const box1 = document.getElementById("first");
+    const box2 = document.getElementById("second");
+    const box3 = document.getElementById("third");
+    gsap.fromTo(
+      box1,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        scrollTrigger: {
+          trigger: "body",
+          start: "top+=20%",
+          end: "top+=40%",
+          scrub: true,
+        },
+      }
+    );
+    gsap.fromTo(
+      box2,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        scrollTrigger: {
+          trigger: "body",
+          start: "top+=30%",
+          end: "top+=50%",
+          scrub: true,
+        },
+      }
+    );
+    gsap.fromTo(
+      box3,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        scrollTrigger: {
+          trigger: "body",
+          start: "top+=40%",
+          end: "top+=60%",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+
   return (
-    <div>
+    <Box height={"7000px"}>
       <Box zIndex={-2} className="landing-background"></Box>
       <Box
         py={"48px"}
@@ -14,36 +63,79 @@ const Home = () => {
         color={"white"}
       >
         <Box
+          width={"40vw"}
+          position={"fixed"}
+          top={"10%"}
           display={"flex"}
           flexDirection={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
+          alignItems={"start"}
           border={"solid 1px white"}
           flexGrow={3}
         >
           <Box
+            id={"first"}
             display={"flex"}
             alignItems={"center"}
             flexGrow={1}
-            border={"solid 1px white"}
+            border={"solid 1px pink"}
           >
-            aaaaaaaaaaaaaaaa
+            <Text fontSize={"4xl"}>나를,</Text>
           </Box>
           <Box
+            id={"second"}
             display={"flex"}
             alignItems={"center"}
             flexGrow={1}
-            border={"solid 1px white"}
+            border={"solid 1px pink"}
           >
-            aaaaaaaaaaaaaaaa
+            <Text fontSize={"4xl"}>그리고</Text>
           </Box>
           <Box
+            id={"third"}
             display={"flex"}
             alignItems={"center"}
             flexGrow={1}
-            border={"solid 1px white"}
+            border={"solid 1px pink"}
           >
-            aaaaaaaaaaaaaaaa
+            <Text fontSize={"4xl"}>나와 맞는 기업을</Text>
+          </Box>
+        </Box>
+        <Box
+          width={"40vw"}
+          position={"fixed"}
+          top={"10%"}
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"end"}
+          border={"solid 1px white"}
+          flexGrow={3}
+        >
+          <Box
+            id={"first"}
+            display={"flex"}
+            alignItems={"center"}
+            flexGrow={1}
+            border={"solid 1px pink"}
+          >
+            <Text fontSize={"4xl"}>나를,</Text>
+          </Box>
+          <Box
+            id={"second"}
+            display={"flex"}
+            alignItems={"center"}
+            flexGrow={1}
+            border={"solid 1px pink"}
+          >
+            <Text fontSize={"4xl"}>그리고</Text>
+          </Box>
+          <Box
+            id={"third"}
+            display={"flex"}
+            alignItems={"center"}
+            flexGrow={1}
+            border={"solid 1px pink"}
+          >
+            <Text fontSize={"4xl"}>나와 맞는 기업을</Text>
           </Box>
         </Box>
         <Box
@@ -56,7 +148,7 @@ const Home = () => {
           키워드
         </Box>
       </Box>
-    </div>
+    </Box>
   );
 };
 
