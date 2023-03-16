9f7e7b6b8918cc3d73e0f594a162db4bbae5b5d9
sunmoon1030 <sunmoon1030@gmail.com>
2023-03-16 10:34:06 +0900

    [Init] Backend 디렉토리 생성

---
+++ backend/src/main/java/com/ssafy/jobtender/JobtenderApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.jobtender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobtenderApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ssafy/jobtender/JobtenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.jobtender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
