c750fccb4c46ac59f29a9e840c7228a30d1dff97
Ubuntu <ubuntu@ip-172-26-4-59.ap-northeast-2.compute.internal>
2023-03-17 01:35:06 +0000

    [Feat] Python : mysql update 추가, 문법 구조 변경
    Former-commit-id: 7bde7ee51cccd609ae6efc55c6dfcdcc2d2e91f7

--- python/crow/getStartBlind.py
+++ python/crow/getStartBlind.py
@@ -12,115 +12,150 @@ def set_time():
     return random.randint(40, 60)
 
 
-def get_company_info(companyName):
-    driver.get('https://www.teamblind.com/kr/company/' + companyName)
+class BlindData:
+    def __init__(self):
+        self.host = '3.38.211.113'
+        self.user = 'jobtender'
+        self.pwd = 'jobtender'
+        self.db = 'jobtender'
+        self.conn = pymysql.connect(host=self.host, user=self.user, password=self.pwd, db=self.db, charset='utf8')
+        self.cur = self.conn.cursor()
+        self.driver = self.webdriver_init()
 
-    intro = list()
-    stars = list()
+    # selenium setting
+    def webdriver_init(self):
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36' \
+                     ' (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('user-agent={0}'.format(user_agent))
+        return webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-    total_rate = driver.find_element(By.CLASS_NAME, 'rate').text
-    total_rate = total_rate.split('\n')[1]
-    stars.append(total_rate)
+    def get_company_info(self, companyName):
+        self.driver.get('https://www.teamblind.com/kr/company/' + companyName)
 
-    # 기업 정보
-    for i in range(2, 7):
-        data = driver.find_element(By.XPATH,
-                                   '/html/body/div/div/div/main/section/div/div/div[2]/div/div/div/section[1]/ul/li[' +
-                                   str(i) + ']').text
-        intro.append(data.split('\n')[1])
+        intro = list()
+        stars = list()
 
-    # 기업 평점 정보
-    for i in range(1, 6):
-        star = driver.find_element(By.XPATH,
-                                   '/html/body/div/div/div/main/section/div/div/div[2]/div/div/div/div[1]/section/'
-                                   'div[1]/div[2]/ul/li[' + str(i) + ']/strong').text
+        total_rate = self.driver.find_element(By.CLASS_NAME, 'rate').text
+        total_rate = total_rate.split('\n')[1]
+        stars.append(total_rate)
 
-        stars.append(star)
+        # 기업 정보
+        for i in range(2, 7):
+            data = self.driver.find_element(By.XPATH,
+                                            '/html/body/div/div/div/main/section/div/div/div[2]/div/div/div/'
+                                            'section[1]/ul/li[' + str(i) + ']').text
+            intro.append(data.split('\n')[1])
 
-    return {'name': companyName,
-            'basic': {'total_rate': total_rate, 'type': intro[0], 'addr': intro[1], 'year': intro[2],
-                      'employees': intro[3], 'salary': intro[4]},
-            'star': {'growth': stars[0], 'balance': stars[1], 'salary_welfare': stars[2],
-                     'culture': stars[3], 'management': stars[4]}}
+        # 기업 평점 정보
+        for i in range(1, 6):
+            star = self.driver.find_element(By.XPATH,
+                                            '/html/body/div/div/div/main/section/div/div/div[2]/div/div/div/'
+                                            'div[1]/section/div[1]/div[2]/ul/li[' + str(i) + ']/strong').text
+
+            stars.append(star)
+
+        return {'name': companyName,
+                'basic': {'total_rate': total_rate, 'type': intro[0], 'addr': intro[1], 'year': intro[2],
+                          'employees': intro[3], 'salary': intro[4]},
+                'star': {'growth': stars[0], 'balance': stars[1], 'salary_welfare': stars[2],
+                         'culture': stars[3], 'management': stars[4]}}
+
+    def find_company(self, companyName):
+        sql_find_company = '''
+            SELECT company_id FROM Companies WHERE name = %s
+            '''
+        result = -1
+        try:
+            self.cur.execute(sql_find_company, companyName)
+            result = self.cur.fetchone()
+            self.conn.commit()
+        except Exception:
+            print(traceback.format_exc())
+
+        return result
+
+    def insert_companies(self, data):
+        sql = '''
+              INSERT INTO `Companies` (name, type, scale, employees_number, address, year_founded, salary)
+              VALUES (%(name)s, %(type)s, %(scale)s, %(employees)s, %(addr)s, %(year)s, %(salary)s);
+              '''
+        try:
+            self.cur.execute(sql, data)
+            self.conn.commit()
+        except Exception:
+            print(traceback.format_exc())
+
+    def insert_rank(self, data):
+        sql = '''
+              INSERT INTO `Company_Ratings` (company_id, average_rating, growth_rating, balance_rating, 
+              salary_welfare_rating, culture_rating, management_rating)
+              VALUES (%(company_id)s, %(total)s, %(growth)s, %(balance)s, %(salary_welfare)s, %(culture)s, %(management)s);
+              '''
+        try:
+            self.cur.execute(sql, data)
+            self.conn.commit()
+        except Exception:
+            print(traceback.format_exc())
+
+    def update_companies(self, data):
+        sql = '''
+         UPDATE `Companies` SET employees_number = %(employees)s, address = %(addr)s,
+         salary = %(salary)s WHERE name = %(name)s;
+        '''
+        try:
+            self.cur.execute(sql, data)
+            self.conn.commit()
+        except Exception:
+            print(traceback.format_exc())
+
+    def update_rank(self, data):
+        sql = '''
+        UPDATE `Company_Ratings` SET average_rating = %(total)s, growth_rating = %(growth)s, balance_rating = %(balance)s,
+        salary_welfare_rating = %(salary_welfare)s, culture_rating = %(culture)s, management_rating = %(management)s
+        WHERE company_id = %(company_id)s;
+        '''
+        try:
+            self.cur.execute(sql, data)
+            self.conn.commit()
+        except Exception:
+            print(traceback.format_exc())
+
+    def end(self):
+        self.conn.close()
+        self.driver.quit()
 
 
 if __name__ == "__main__":
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36' \
-                 ' (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-
-    # selenium setting
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('user-agent={0}'.format(user_agent))
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-    # db setting
-    host = '3.38.211.113'
-    user = 'jobtender'
-    pwd = 'jobtender'
-    db = 'jobtender'
+    b = BlindData()
 
     # get company names
     f = open('blindCompanyName.txt', 'r', encoding='utf-8')
     lines = f.readlines()
-
     # lines = ['삼성전자']
-    conn = pymysql.connect(host=host, user=user, password=pwd, db=db, charset='utf8')
-    a = 0
     for name in lines:
-        if a == 6:
-            break
-        a += 1
         name = name.replace("\n", "")
-        try:
-            info = get_company_info(name)  # 기업 정보를 json 형태로 return
-            print(info)
 
-            cur = conn.cursor()
-            basic = {'name': info['name'], 'type': info['basic']['type'], 'scale': '대기업',
-                     'employees': info['basic']['employees'],
-                     'addr': info['basic']['addr'], 'year': info['basic']['year'], 'salary': info['basic']['salary']}
+        info = b.get_company_info(name)
 
-            sql_companies = '''
-                            INSERT INTO `Companies` (name, type, scale, employees_number, address, year_founded, salary)
-                            VALUES (%(name)s, %(type)s, %(scale)s, %(employees)s, %(addr)s, %(year)s, %(salary)s);
-                            '''
-            try:
-                cur.execute(sql_companies, basic)
-                conn.commit()
-            except Exception as e1:
-                print(traceback.format_exc())
-                break
+        basic = {'name': info['name'], 'type': info['basic']['type'], 'scale': '대기업',
+                 'employees': info['basic']['employees'],
+                 'addr': info['basic']['addr'], 'year': info['basic']['year'], 'salary': info['basic']['salary']}
 
-            company_no = '''
-                          SELECT company_id FROM Companies WHERE name = %s;
-                         '''
-            try:
-                company_no = cur.execute(company_no, name)
-                conn.commit()
-            except Exception as e2:
-                print(traceback.format_exc())
-                break
+        star = {'company_id': b.find_company(name), 'total': info['basic']['total_rate'],
+                'growth': info['star']['growth'], 'balance': info['star']['balance'],
+                'salary_welfare': info['star']['salary_welfare'], 'culture': info['star']['culture'],
+                'management': info['star']['management']}
 
-            star = [company_no, info['basic']['total_rate'], info['star']['growth'], info['star']['balance'],
-                    info['star']['salary_welfare'], info['star']['culture'], info['star']['management']]
-            sql_ratings = '''
-                          INSERT INTO `Company_Ratings` (company_id, average_rating, growth_rating, balance_rating, 
-                          salary_welfare_rating, culture_rating, management_rating)
-                          VALUES (%s, %s, %s, %s, %s, %s, %s);
-                          '''
-            try:
-                cur.execute(sql_ratings, star)
-                conn.commit()
-            except Exception as e3:
-                print(traceback.format_exc())
-                break
-        except Exception as e:
-            print(e)
-            continue
+        if star['company_id'] is None:  # insert
+            b.insert_companies(basic)
+            star['company_id'] = b.find_company(name)
+            b.insert_rank(star)
+        else:  # update
+            b.update_companies(basic)
+            b.update_rank(star)
         time.sleep(set_time())
-
-    conn.close()
-    driver.quit()
+    b.end()
