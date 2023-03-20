db460cc3d16db9222612ca3268ef535db0749c20
socm9145 <socm9145@naver.com>
2023-03-20 17:52:31 +0900

    [Feat] Similar Company Dao : 유사 기업 이름 리스트 반환 구현

--- backend/src/main/java/com/ssafy/jobtender/repo/SimilarCompanyRepo.java
+++ backend/src/main/java/com/ssafy/jobtender/repo/SimilarCompanyRepo.java
@@ -4,6 +4,10 @@ import com.ssafy.jobtender.entity.SimilarCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SimilarCompanyRepo extends JpaRepository<SimilarCompany, Long> {
+    Optional<List<SimilarCompany>> findAllByCompanyId(long company_id);
 }
--- backend/src/main/java/com/ssafy/jobtender/service/SimilarCompanyService.java
+++ backend/src/main/java/com/ssafy/jobtender/service/SimilarCompanyService.java
@@ -1,4 +1,9 @@
 package com.ssafy.jobtender.service;
 
+import com.ssafy.jobtender.dto.output.ComparableCompanyNameOutputDTO;
+
+import java.util.List;
+
 public interface SimilarCompanyService {
+    List<ComparableCompanyNameOutputDTO> readComparableCompanies(long selected_company_id);
 }
