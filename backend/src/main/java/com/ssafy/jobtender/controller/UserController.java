5fe77c5adce44154215496f05f77e06ed5940715
sunmoon1030 <sunmoon1030@gmail.com>
2023-03-20 17:00:04 +0900

    [Feat] InputKeyWord : controller, service, dao 초안

--- backend/src/main/java/com/ssafy/jobtender/controller/UserController.java
+++ backend/src/main/java/com/ssafy/jobtender/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.ssafy.jobtender.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.jobtender.dto.input.KeywordInputDTO;
+import com.ssafy.jobtender.service.InputService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final InputService inputService;
+
+    @Autowired
+    public UserController(InputService inputService) {
+        this.inputService = inputService;
+    }
+    @ResponseBody
+    @PostMapping("/keyword")
+    public void createInputsKeyword(@RequestBody KeywordInputDTO keywordInputDTO){
+        List<String> userKeyWord = keywordInputDTO.getKeyWords();
+
+    }
 }
