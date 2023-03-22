5d46cb7f80ca3d43365763983e4ddd2da75c415d
유정훈 <choanury@naver.com>
2023-03-22 17:58:02 +0900

    [Feat] Homepage card carousel Home page 기본 레이아웃과 카드 캐러셀 기능 추가
    Former-commit-id: 5c1a0af7978a12cf6cc1fa92eb5d88b460232449

--- frontend/src/components/home/ImageContainer.js
+++ frontend/src/components/home/ImageContainer.js
@@ -1,6 +1,66 @@
-import { Box, Image } from "@chakra-ui/react";
+import "../../styles/ImageContainer.css";
 
+import React, { useEffect, useRef } from "react";
+
+import { Box, Image } from "@chakra-ui/react";
 const ImageContainer = () => {
+  const carouselRef = useRef(null);
+
+  const handleMouseDown = (event) => {
+    const carousel = carouselRef.current;
+    if (carousel.classList.contains("transition")) return;
+    const dragStart = event.pageX;
+
+    const handleMouseMove = (event) => {
+      const dragEnd = event.pageX;
+      carousel.style.transform = "translateX(" + (dragEnd - dragStart) + "px)";
+    };
+
+    const handleMouseUp = (event) => {
+      const dragEnd = event.pageX;
+      const dragPos = dragEnd - dragStart;
+
+      if (dragPos > 150) {
+        shiftSlide(1);
+      } else if (dragPos < -150) {
+        shiftSlide(-1);
+      } else {
+        shiftSlide(0);
+      }
+
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp, { once: true });
+  };
+
+  const shiftSlide = (direction) => {
+    const carousel = carouselRef.current;
+    if (carousel.classList.contains("transition")) return;
+    carousel.classList.add("transition");
+    carousel.style.transform = "translateX(" + direction * 830 + "px)";
+
+    setTimeout(() => {
+      const slides = carousel.querySelectorAll(".slide");
+      if (direction === 1) {
+        carousel.insertBefore(slides[slides.length - 1], slides[0]);
+      } else if (direction === -1) {
+        carousel.appendChild(slides[0]);
+      }
+      carousel.classList.remove("transition");
+      carousel.style.transform = "translateX(0px)";
+    }, 700);
+  };
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+    carousel.addEventListener("mousedown", handleMouseDown);
+    return () => {
+      carousel.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
   return (
     <Box
       height={"100%"}
@@ -14,29 +74,35 @@ const ImageContainer = () => {
         justifyContent={"space-between"}
         width={"100vw"}
       >
-        <Box
-          flexGrow={"2"}
-          height={"100%"}
-          overflow={"hidden"}
-          marginRight={"2em"}
-        >
-          <Image
-            height={"100%"}
-            objectFit={"cover"}
-            src={process.env.PUBLIC_URL + "/images/keywords-image.jpg"}
-          ></Image>
-        </Box>
-        <Box
-          flexGrow={"1"}
-          height={"100%"}
-          overflow={"hidden"}
-          marginRight={"2em"}
-        >
-          <Image
-            height={"100%"}
-            objectFit={"cover"}
-            src={process.env.PUBLIC_URL + "/images/survey-image.jpg"}
-          ></Image>
+        <Box className="wrap">
+          <Box className="window">
+            <Box id="carousel" ref={carouselRef} display={"inline-block"}>
+              <Box
+                className="slide"
+                id="b1"
+                marginRight={"30px"}
+                backgroundImage="url('/images/keywords-image.jpg')"
+              ></Box>
+              <Box
+                className="slide"
+                id="b2"
+                marginRight={"30px"}
+                backgroundImage="url('/images/survey-image.jpg')"
+              ></Box>
+              <Box
+                className="slide"
+                id="b1"
+                marginRight={"30px"}
+                backgroundImage="url('/images/keywords-image.jpg')"
+              ></Box>
+              <Box
+                className="slide"
+                id="b2"
+                marginRight={"30px"}
+                backgroundImage="url('/images/survey-image.jpg')"
+              ></Box>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
