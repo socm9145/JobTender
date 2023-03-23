97131453ed08c4e8b473cf5ecec20eb11de092a2
socm9145 <socm9145@naver.com>
2023-03-23 17:16:46 +0900

    [Fix] Keyword Service : 어노테이션 수정

--- backend/src/main/java/com/ssafy/jobtender/service/impl/KeywordServiceImpl.java
+++ backend/src/main/java/com/ssafy/jobtender/service/impl/KeywordServiceImpl.java
@@ -3,10 +3,12 @@ package com.ssafy.jobtender.service.impl;
 import com.ssafy.jobtender.dao.KeywordDAO;
 import com.ssafy.jobtender.entity.Keyword;
 import com.ssafy.jobtender.service.KeywordService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class KeywordServiceImpl implements KeywordService {
     private final KeywordDAO keywordDAO;
     public KeywordServiceImpl (KeywordDAO keywordDAO){
