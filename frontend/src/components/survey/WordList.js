160282d3557c739af82ee2a0edf6fe01f0af5f1e
dongsu712 <dongsu712@naver.com>
2023-03-23 17:53:51 +0900

    [Feat] Survey 페이지 기능
    Former-commit-id: b0eda01c254ce0b3f5053ca45187cff95f874c84

---
+++ frontend/src/components/survey/WordList.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+
+import "../../styles/WordList.css";
+
+const words = [
+  "apple",
+  "banana",
+  "cat",
+  "dog",
+  "elephant",
+  "forest",
+  "giraffe",
+  "house",
+  "ice",
+  "juice",
+  "kangaroo",
+  "lion",
+  "mountain",
+  "night",
+  "ocean",
+  "penguin",
+  "queen",
+  "rain",
+  "snow",
+  "tiger",
+  "umbrella",
+  "violin",
+  "whale",
+  "xylophone",
+  "yarn",
+  "zebra",
+  "airplane",
+  "bear",
+  "crocodile",
+  "dolphin",
+  "eagle",
+  "flower",
+  "goat",
+  "hedgehog",
+  "island",
+  "jacket",
+  "koala",
+  "lamp",
+  "monkey",
+  "nest",
+  "orange",
+  "piano",
+  "quail",
+  "river",
+  "star",
+  "turtle",
+  "unicorn",
+  "volcano",
+  "window",
+  "x-ray",
+  "yak",
+  "zipper",
+  "ant",
+  "butterfly",
+  "camel",
+  "dragonfly",
+  "elephant",
+  "fox",
+];
+
+const WordList = () => {
+  const [wordScores, setWordScores] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animated, setAnimated] = useState(true);
+
+  const handleScoreChange = (word, score) => {
+    setWordScores({ ...wordScores, [word]: score });
+    setTimeout(() => {
+      handleNextWord();
+    }, 100);
+  };
+
+  const handleNextWord = () => {
+    const nextIndex = currentIndex + 1;
+    if (nextIndex < words.length) {
+      setCurrentIndex(nextIndex);
+    }
+  };
+
+  const handlePrevWord = () => {
+    const prevIndex = currentIndex - 1;
+    if (prevIndex >= 0) {
+      const prevWordScore = wordScores[words[prevIndex]];
+      if (prevWordScore !== undefined) {
+        setCurrentIndex(prevIndex);
+      }
+    }
+  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [currentIndex]);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      if (e.deltaY > 0) {
+        if (
+          currentIndex < words.length - 1 &&
+          wordScores[words[currentIndex]]
+        ) {
+          handleNextWord();
+        }
+      } else {
+        if (currentIndex > 0) {
+          handlePrevWord();
+        }
+      }
+    };
+    window.addEventListener("wheel", handleScroll);
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [currentIndex]);
+
+  const currentWord = words[currentIndex];
+
+  return (
+    <div className="survey-wrapper">
+      <div className="slot-container">
+        <div
+          className={`slot-word previous-word ${
+            currentIndex > 0 ? "animated" : ""
+          }`}
+        >
+          {words[currentIndex - 1]}
+        </div>
+        <div className={`slot-word current-word ${animated ? "animated" : ""}`}>
+          {currentWord}
+          <div className="score-container">
+            {[1, 2, 3, 4, 5, 6, 7].map((score) => (
+              <button
+                key={score}
+                className="score-button"
+                onClick={() => handleScoreChange(currentWord, score)}
+              >
+                {score}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div
+          className={`slot-word next-word ${
+            currentIndex < words.length - 1 ? "animated" : ""
+          }`}
+        >
+          {words[currentIndex + 1]}
+        </div>
+      </div>
+      <div className="navigation-buttons">
+        <button disabled={currentIndex === 0} onClick={handlePrevWord}>
+          &lt;
+        </button>
+        <button
+          disabled={
+            currentIndex === words.length - 1 ||
+            !wordScores[words[currentIndex]]
+          }
+          onClick={handleNextWord}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default WordList;
--- frontend/src/pages/router/Survey.js
+++ frontend/src/pages/router/Survey.js
@@ -1,5 +1,21 @@
+import WordList from "../../components/survey/WordList";
+
+import { Box, Text } from "@chakra-ui/react";
+
 const Survey = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        height={"100vh"}
+      >
+        <WordList />
+      </Box>
+    </div>
+  );
 };
 
 export default Survey;
