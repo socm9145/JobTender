1de85ae4dda32a31cf67f2b799b22ce3eba2e366
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2023-03-31 14:46:25 +1000

    [Feat] change controller

--- backend/src/main/java/com/ssafy/jobtender/controller/AccountController.java
+++ backend/src/main/java/com/ssafy/jobtender/controller/AccountController.java
@@ -9,15 +9,5 @@ import java.util.List;
 @RestController
 @RequestMapping("/account")
 public class AccountController {
-//    @ApiOperation(
-//            value = "여러 member 생성"
-//            , notes = "기본 데이터로 여러 member 생성")
-//    @PostMapping("/accounts")
-//    public ResponseEntity<List<MemberDTO>> createMembers(@RequestBody List<MemberInputDTO> memberInputDtos) throws Exception {
-//        List<MemberDTO> rMemberDtos = memberService.insertMembers(memberInputDtos);
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(rMemberDtos);
-//    }
-
 
 }
