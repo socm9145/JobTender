fef1c45046e311d514ce11aeac62cb46f016910a
김동수 <dongsu712@naver.com>
2023-04-03 22:26:43 +0900

    [Feat] Result 페이지 5번차트 api 통신
    Former-commit-id: 9a161d61fbc2e28b2423da8c4c9cc51a33c03834

--- frontend/src/components/result/5/AgeValues.js
+++ frontend/src/components/result/5/AgeValues.js
@@ -1,47 +1,114 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 import * as math from "mathjs";
 
 import GroupedBarChart from "./GroupedBarChart";
 import dataSets from "./barData.json";
 
-const AgeValues = () => {
-  const [chartData, setChartData] = useState(dataSets[0]);
-  const averageData = dataSets[10];
+import { localServer } from "../../../api/http";
 
-  const [result, setResult] = useState(0);
-  const [upDownCheck, setUpDownCheck] = useState("");
+const api = localServer();
+
+const AgeValues = () => {
+  // 차트데이터
+  const dummyData = [
+    { letter: "남", frequency: 0 },
+    { letter: "여", frequency: 0 },
+    { letter: "나", frequency: 0 },
+  ];
+  const [chartData, setChartData] = useState(dummyData);
+  const [upDownCheckF, setUpDownCheckF] = useState("");
+  const [upDownCheckM, setUpDownCheckM] = useState("");
+  const [resultF, setResultF] = useState(0);
+  const [resultM, setResultM] = useState(0);
+
+  const [maleMean, setMaleMean] = useState(0);
+  const [maleStd, setMaleStd] = useState(0);
+  const [femaleMean, setFemaleMean] = useState(0);
+  const [femaleStd, setFemaleStd] = useState(0);
+  const [myAverage, setMyAverage] = useState(5);
+
   const changeData = (index) => {
-    setChartData(dataSets[index]);
+    // 남자 데이터 받아오기
+    api
+      .get(`/result/static?keywordId=${index}&gender=M`)
+      .then(function (response) {
+        // console.log(response.data);
+        // console.log(response.data.mean);
+        setMaleMean(response.data.mean);
+        setMaleStd(response.data.std);
+      });
+    // 여자 데이터 받아오기.
+    api
+      .get(`/result/static?keywordId=${index}&gender=F`)
+      .then(function (response) {
+        // console.log(response.data);
+        // console.log(response.data.mean);
+        setFemaleMean(response.data.mean);
+        setFemaleStd(response.data.std);
+      });
+
+    // 내 값 받아오기
+    api
+      .get(
+        // `/result/survey/average?resultId=${리절트아이디}&keyworldId=${index}`
+        `/result/survey/average?resultId=1&keyworldId=${index}`
+      )
+      .then(function (response) {
+        setMyAverage(parseInt(response.data.average));
+      });
+
+    const newData = [
+      { letter: "남", frequency: maleMean },
+      { letter: "여", frequency: femaleMean },
+      { letter: "나", frequency: myAverage },
+    ];
+
+    setChartData(newData);
   };
 
-  let a = "adfsdf";
-  const phozun = (i) => {
-    const mean = averageData[i].mean;
-    const std_dev = averageData[i].std_dev;
-    const score = averageData[i].score;
-    const z_score = (score - mean) / std_dev;
+  const standardF = (i, std, mean, score) => {
+    const z_score = (score - mean) / std;
 
     // 정규분포에서 유저 스코어의 위치 계산
     const cdf_value = (1 + math.erf(z_score / Math.sqrt(2))) / 2;
 
     // 퍼센타일 계산
     const percentile = cdf_value * 100;
-    console.log(percentile);
     // 상위 50퍼 안이면 상위로 표시. 아니면 하위로 표시
     if (percentile >= 50) {
       // const result = 100 - percentile;
-      setResult((100 - percentile).toFixed(2));
-      setUpDownCheck("상위");
-      console.log(`상위 ${result}% 입니다`);
+      setResultF((100 - percentile).toFixed(2));
+      setUpDownCheckF("여성 데이터 기준 상위");
     } else {
-      console.log(`하위 ${percentile}% 입니다`);
-      setResult(percentile.toFixed(2));
-      setUpDownCheck("하위");
+      setResultF(percentile.toFixed(2));
+      setUpDownCheckF("여성 데이터 기준 하위");
+    }
+  };
+  const standardM = (i, std, mean, score) => {
+    const z_score = (score - mean) / std;
+    console.log(std, mean, score);
+    // 정규분포에서 유저 스코어의 위치 계산
+    const cdf_value = (1 + math.erf(z_score / Math.sqrt(2))) / 2;
+
+    // 퍼센타일 계산
+    const percentile = cdf_value * 100;
+    // 상위 50퍼 안이면 상위로 표시. 아니면 하위로 표시
+    if (percentile >= 50) {
+      // const result = 100 - percentile;
+      setResultM((100 - percentile).toFixed(2));
+      setUpDownCheckM("남성 데이터 기준 상위");
+    } else {
+      setResultM(percentile.toFixed(2));
+      setUpDownCheckM("남성 데이터 기준 하위");
     }
   };
 
-  console.log(chartData);
+  console.log(upDownCheckF);
+  console.log(upDownCheckM);
+  console.log(resultM);
+  console.log(resultF);
+
   return (
     <div>
       <GroupedBarChart data={chartData} />
@@ -52,7 +119,8 @@ const AgeValues = () => {
               key={i}
               onClick={() => {
                 changeData(i);
-                phozun(i);
+                standardF(i, femaleStd, femaleMean, myAverage);
+                standardM(i, maleStd, maleMean, myAverage);
               }}
             >
               {i + 1}번
@@ -60,8 +128,10 @@ const AgeValues = () => {
           </div>
         ))}
         <p>
-          {upDownCheck}
-          {result} % 입니다.
+          {upDownCheckF}
+          {resultF} % 입니다.
+          {upDownCheckM}
+          {resultM} % 입니다.
         </p>
       </div>
     </div>
