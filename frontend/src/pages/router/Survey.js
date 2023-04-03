c18484f1d4648a9259360198c2128b1e24a80396
김동수 <dongsu712@naver.com>
2023-04-03 20:54:59 +0900

    [Refac] survey 폴더 필요없는 컴포넌트 정리
    Former-commit-id: e5c5ec969074e23763fd377f66dde5847f03129d

--- frontend/src/pages/router/Survey.js
+++ frontend/src/pages/router/Survey.js
@@ -1,11 +1,4 @@
-import WordList from "../../components/survey/WordList";
-
 import { Box, Text } from "@chakra-ui/react";
-import WordAnima from "../../components/survey/WordAnima";
-import WordAnima2 from "../../components/survey/WordAnima2";
-import WordAnima3 from "../../components/survey/WordAnima3";
-import WordSlider from "../../components/survey/WordSlider";
-import WordSlider2 from "../../components/survey/WordSlider2";
 import WordSlide from "../../components/survey/WordSlide";
 
 const Survey = () => {
@@ -20,12 +13,6 @@ const Survey = () => {
         height={"100vh"}
         width={"100vw"}
       >
-        {/* <WordList /> */}
-        {/* <WordAnima /> */}
-        {/* <WordAnima2 /> */}
-        {/* <WordAnima3 /> */}
-        {/* <WordSlider /> */}
-        {/* <WordSlider2 /> */}
         <WordSlide />
       </Box>
     </div>
