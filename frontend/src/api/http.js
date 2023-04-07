b44380501bef5a946eed30cd498bd3487ec2b32b
socm9145 <socm9145@naver.com>
2023-04-07 11:01:20 +0900

    [Build] 프론트엔드 axios URL 변경
    Former-commit-id: e6231b4a3ba66715da967119cdfd978a47ff6d28

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getCookie = async () => {
   try {
-    const { data } = await axios.get("http://localhost:8000/account/reissue", {
+    const { data } = await axios.get("https://jobtender.shop/api/account/reissue", {
       withCredentials: true,
     });
     sessionStorage.setItem("userId", data);
@@ -16,8 +16,8 @@ const getCookie = async () => {
 function localServer() {
   // console.log(process.env.REACT_APP_SOCKET);
   const axiosConfig = {
-    // baseURL: 'https://jobtender.shop/api',
-    baseURL: "http://localhost:8000/",
+    baseURL: 'https://jobtender.shop/api',
+    // baseURL: "http://localhost:8000/",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
@@ -62,7 +62,7 @@ function localServer() {
 
 function loginServer() {
   const axiosConfig = {
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://jobtender.shop/api/",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
@@ -72,7 +72,7 @@ function loginServer() {
 
 function pythonServer() {
   const axiosConfig = {
-    baseURL: "http://localhost:8001/",
+    baseURL: "https://jobtender.shop/api/v2/",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
