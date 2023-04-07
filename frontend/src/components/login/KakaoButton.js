1ab89dd24a71aa22e0eae6e2df8b69ec4b993394
socm9145 <socm9145@naver.com>
2023-04-07 11:34:16 +0900

    [Fix] 카카오 로그인 url 수정
    Former-commit-id: 8b31ccd935a8f427d63a32e749f504f9ef2ecdb9

--- frontend/src/components/login/KakaoButton.js
+++ frontend/src/components/login/KakaoButton.js
@@ -3,6 +3,7 @@ import { Box, Image } from "@chakra-ui/react";
 const KakaoButton = ({ buttonWidth }) => {
   const REST_API_KEY = "1d00d14f1ffe2865a5b8a876c3de14da";
   const REDIRECT_URI = "https://jobtender.shop/kakao";
+  // const REDIRECT_URI = "http://localhost:3000/kakao";
   const kakaoLogin = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
 
   return (
